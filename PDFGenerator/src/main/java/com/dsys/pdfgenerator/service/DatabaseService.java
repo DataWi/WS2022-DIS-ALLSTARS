b19bce3640e88ec028499a33402fd6045e8c2912
DataWi <wi21b094@technikum-wien.at>
2023-09-02 12:53:31 +0200

    functionality complete, unit tests missing

--- PDFGenerator/src/main/java/com/dsys/pdfgenerator/service/DatabaseService.java
+++ PDFGenerator/src/main/java/com/dsys/pdfgenerator/service/DatabaseService.java
@@ -3,7 +3,6 @@ package com.dsys.pdfgenerator.service;
 import com.dsys.pdfgenerator.model.Customer;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class DatabaseService {
     private static Connection connect() throws SQLException {
@@ -14,8 +13,7 @@ public class DatabaseService {
 
 
     public static Customer getCustomer(String id) throws SQLException {
-         Customer customer = new Customer(0, "Jane", "Does");
-
+        Customer customer;
         try ( Connection conn = connect() ) {
             String query = "SELECT id, first_name, last_name FROM customer WHERE id = " + id + ";";
             PreparedStatement stmt = conn.prepareStatement(query);
@@ -23,13 +21,11 @@ public class DatabaseService {
 
             if(!resultSet.next()) {
                 throw new SQLException("Customer not found in DB");
-            }
-
-            while( resultSet.next()) {
+            } else {
                  customer = new Customer(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
+                        resultSet.getInt("id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
                 );
             }
             return customer;
