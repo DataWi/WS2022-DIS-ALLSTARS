5a2099c342fffd85e1f0b19f5d5f31eaa5143e76
wi21b095 <wi21b095@technikum-wien.at>
2023-09-03 15:01:08 +0200

    Download Button und Action hinzugefügt

--- StationUI/src/main/java/com/example/stationui/TankstellenUIController.java
+++ StationUI/src/main/java/com/example/stationui/TankstellenUIController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -44,6 +47,64 @@ public class TankstellenUIController {
              alert.showAndWait();
          }
      }
+    @FXML
+    private void onButtonDownloadPDFClicked(ActionEvent event) {
+        // Holen Sie sich den Kunden-ID-Wert aus dem Textfeld
+        String customerID = customerIDField.getText();
+
+        // Hier wird der Dateiname basierend auf der Kunden-ID erstellt
+        String fileName = "Invoice_" + customerID + ".pdf";  // Beispiel-Dateiname
+
+        try {
+            // Erstellen Sie eine HTTP-Anfrage, um die PDF-Datei herunterzuladen
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(API_URL + customerID + "/download"))
+                    .build();
+
+            // Senden Sie die Anfrage und erhalten Sie die Antwort
+            HttpResponse<java.io.InputStream> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+            // Überprüfen Sie, ob die Anfrage erfolgreich war (Statuscode 200)
+            if (response.statusCode() == 200) {
+                // Pfad, in dem die heruntergeladene PDF-Datei gespeichert wird
+                // Holen Sie sich den Benutzerverzeichnis-Pfad
+                String userHome = System.getProperty("user.home");
+
+            // Pfad zum "Downloads"-Ordner hinzufügen
+                String downloadFolderPath = userHome + File.separator + "Downloads";
+
+            // Erstellen Sie den vollständigen Pfad zum Speichern der heruntergeladenen PDF-Datei
+                String downloadFilePath = downloadFolderPath + File.separator + fileName;
+
+                // Schreiben Sie die heruntergeladene PDF-Datei in das Dateisystem
+                try (BufferedInputStream inputStream = new BufferedInputStream(response.body());
+                     FileOutputStream fileOutputStream = new FileOutputStream(downloadFilePath)) {
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+
+                // Zeigen Sie eine Erfolgsmeldung an
+                Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+                successAlert.setContentText("PDF-Datei erfolgreich heruntergeladen.");
+                successAlert.showAndWait();
+            } else {
+                // Zeigen Sie eine Fehlermeldung an, wenn die Anfrage nicht erfolgreich war
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setContentText("Fehler beim Herunterladen der PDF-Datei.");
+                errorAlert.showAndWait();
+            }
+        } catch (Exception e) {
+            // Zeigen Sie eine Fehlermeldung an, wenn ein Fehler auftritt
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Fehler beim Aufrufen der REST-API: \n" + e.toString());
+            alert.showAndWait();
+        }
+    }
 
     public void onButtonCancelClicked(ActionEvent actionEvent) {
         System.exit(0);
